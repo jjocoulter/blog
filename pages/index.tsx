4df96f8e58255326a56fc221609dfff0fdef9c08
Josh Coulter <u0861925@unimail.hud.ac.uk>
2021-06-22 22:35:48 +0100

    implemented login authentication

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Loader from "@components/Loader";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Loader show />
+    </div>
+  );
 }
